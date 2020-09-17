eb704994ce2dd7096e0436dfc8fe4943d6b3cef9
Chrystian Bee <chrystianpaulino@beedelivery.com.br>
2020-09-17 18:27:45 -0300

    organziacao nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-// any para paginacao
-Route::any('/admin/plans/search'        , 'Admin\PlanController@search'     )->name('plans.search');
+Route::prefix('admin')->namespace('Admin')->group(function (){
 
-Route::get('admin/plans'                , 'Admin\PlanController@index'      )->name('plans.index');
-Route::get('admin/plans/create'         , 'Admin\PlanController@create'     )->name('plans.create');
-Route::post('admin/plans'               , 'Admin\PlanController@store'      )->name('plans.store');
-Route::get('admin/plans/{url}'          , 'Admin\PlanController@show'       )->name('plans.show');
-Route::delete('admin/plans/{url}'       , 'Admin\PlanController@destroy'    )->name('plans.destroy');
-Route::get('admin/plans/{url}/edit'     , 'Admin\PlanController@edit'       )->name('plans.edit');
-Route::put('admin/plans/{url}'          , 'Admin\PlanController@update'     )->name('plans.update');
+    // any para paginacao
+    Route::any('plans/search'           , 'PlanController@search'       )->name('plans.search');
 
-Route::get('admin'                      , 'Admin\PlanController@index'      )->name('admin.index');
+    Route::get('plans'                  , 'PlanController@index'        )->name('plans.index');
+    Route::get('plans/create'           , 'PlanController@create'       )->name('plans.create');
+    Route::post('plans'                 , 'PlanController@store'        )->name('plans.store');
+    Route::get('plans/{url}'            , 'PlanController@show'         )->name('plans.show');
+    Route::delete('plans/{url}'         , 'PlanController@destroy'      )->name('plans.destroy');
+    Route::get('plans/{url}/edit'       , 'PlanController@edit'         )->name('plans.edit');
+    Route::put('plans/{url}'            , 'PlanController@update'       )->name('plans.update');
+
+    Route::get('/'                      , 'Admin\PlanController@index'  )->name('admin.index');
+
+});
 
 Route::get('/', function () {
     return view('welcome');
